40fe44d64a379b5a0ae03efd546a486b4e3d7673
Avandrel <m.choroszy@mgpm.pl>
2015-12-26 21:47:56 +0100

    Create cards from slack

--- lib/web/slack_trello.rb
+++ lib/web/slack_trello.rb
@@ -1,26 +1,49 @@
+# encoding UTF-8
+
+require 'trello'
+
 module DiTrello
 	class SlackTrello
-		def initialize
+		def initialize(inbox_list)
 			@config_hash = DiTrello::Config.get_config_hash
+			@inbox_list = inbox_list
 		end
 
 		def respond(params)
 			if !validate_token(params['token'])
-				return get_message('Invalid token!')
+				#return get_message('Błędny token!')
 			end
 
-			conc_params = ''
-	      	params.each do |key,value|
-		        if value.is_a? String
-	          	conc_params = "#{conc_params}; #{key}: #{value}"
-	        	end
-	      	end
+			raw_input = params['text'].sub(params['trigger_word'], "").strip
+			trello_result = create_card(raw_input, params['user_name'])
+			get_message(trello_result)
 		end
 
 		private
 
 		def validate_token(token)
 			@config_hash['slack_wywiad_outgoing_token'] == token
+		end
+
+		def create_card(message, user_name)
+			if !@inbox_list.cards.any? { |card| card.name == message}
+          		card = Trello::Card.create({
+            	:list_id => @inbox_list.id,
+            	:name => message
+          		})
+          		card.save
+          		return create_ok_message(user_name)
+          	else
+          		return create_already_exists_message(user_name)
+        	end
+		end
+
+		def create_ok_message(user)
+			"#{user}: Dziękujemy zagłoszenie!"
+		end
+
+		def create_already_exists_message(user)
+			"#{user}: *Takie zgłoszenie już istnieje!*"
 		end
 
 		def get_message(message)
--- lib/web/web.rb
+++ lib/web/web.rb
@@ -10,19 +10,15 @@ module DiTrello
     before do
         headers "Content-Type" => "application/json; charset=utf8"
         @config_hash = DiTrello::Config.get_config_hash
+
+        Trello.configure do |config|
+          config.developer_public_key = @config_hash['trello_developer_public_key']
+          config.member_token = @config_hash['trello_member_token']
+        end
+        @inbox_list = Trello::Board.find(@config_hash['trello_board_id']).lists.first { |list| list.name = "Inbox"}
     end
 
     get "/" do
-      Trello.configure do |config|
-        config.developer_public_key = @config_hash['trello_developer_public_key']
-        config.member_token = @config_hash['trello_member_token']
-      end
-
-      board = Trello::Board.find(@config_hash['trello_board_id'])
-      inbox_list =  board.lists.first { |list| list.name = "Inbox"}
-      #puts inbox_list.cards.inspect
-      
-
       if ENV['RACK_ENV'] != 'production'
         client = DiscourseApi::Client.new(@config_hash['discourse_url'])
         client.api_key = @config_hash['discourse_api_key']
@@ -31,9 +27,9 @@ module DiTrello
         messages = client.private_messages(client.api_username)
         puts messages.to_json
         messages.each do |message|
-          if !inbox_list.cards.any? { |card| card.name == message["title"]}
+          if !@inbox_list.cards.any? { |card| card.name == message["title"]}
             card = Trello::Card.create({
-              :list_id => inbox_list.id,
+              :list_id => @inbox_list.id,
               :name => message["title"]
             })
             card.save
@@ -44,9 +40,8 @@ module DiTrello
     end
 
     post "/create" do
-      slack_trello = DiTrello::SlackTello.new
-
-      slack_trello.respond
+      slack_trello = DiTrello::SlackTrello.new @inbox_list
+      slack_trello.respond(params)
     end
   end
 end
