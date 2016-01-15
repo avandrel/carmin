f50c550bc76af8f34725692fffa81f514c0ab791
Avandrel <m.choroszy@mgpm.pl>
2016-01-15 23:40:36 +0100

    Add missing file

---
+++ lib/repositories/body_repository.rb
@@ -0,0 +1,22 @@
+require 'mongo'
+
+module Carmin
+	class BodyRepository
+		def initialize(mongo_helper)
+			@mongo_helper = mongo_helper
+		end
+
+		def body_in_repo?(date)
+			puts date
+            @mongo_helper.body_collection.find({:date => "#{date}"}).first() != nil
+        end
+
+		def add_body(body)
+        	@mongo_helper.body_collection.insert_one(body)
+        end
+
+        def update_body(body)
+        	@mongo_helper.body_collection.update_one({:date => "#{body[:date]}"}, body)
+        end
+	end
+end
